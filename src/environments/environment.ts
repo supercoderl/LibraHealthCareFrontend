4381b28d5d6dabaec4407dd1f8a82ab351e3226f
supercoderl <150410325+supercoderl@users.noreply.github.com>
2025-09-26 16:23:18 +0700

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
     useHash: false,
     api: {
         // baseUrl: 'https://localhost:7261',
-        baseUrl: 'https://libra-healthcare.ddns.net',
+        baseUrl: 'https://libra-healthcare.somee.com',
         refreshTokenEnabled: true,
         refreshTokenType: 'auth-refresh'
     },
